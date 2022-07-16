bd6ef545e2580599b0c10d8787546d5710c5ca29
Mohamed-Rajab-2112 <mrajab2112@gmail.com>
2022-07-16 04:44:41 +0200

    feat: add autocomplete search functionality

---
+++ src/store/store.ts
@@ -0,0 +1,15 @@
+import {Action, configureStore, ThunkAction} from '@reduxjs/toolkit'
+import weatherReducer from "store/features/weather/weatherSlice"
+
+export const store = configureStore({
+	reducer: {
+		weather: weatherReducer
+	}
+})
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppThunk<ReturnType = void> = ThunkAction<ReturnType,
+	RootState,
+	unknown,
+	Action<string>>;
