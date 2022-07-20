405ef6c255ac9c3dcc52e03381b5875035b85384
mohamedragab <mrajab2112@gmail.com>
2022-07-20 14:46:10 +0200

    feat: render weather cards and delete/edit them

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {createRoot} from 'react-dom/client'
+import Modal from 'react-modal'
 import {Provider} from 'react-redux'
 import {store} from 'store/store'
 import App from './App'
@@ -8,13 +9,12 @@ import reportWebVitals from './reportWebVitals'
 
 const container = document.getElementById('root')!
 const root = createRoot(container)
+Modal.setAppElement('#root')
 
 root.render(
-	<React.StrictMode>
-		<Provider store={store}>
-			<App />
-		</Provider>
-	</React.StrictMode>
+	<Provider store={store}>
+		<App />
+	</Provider>
 )
 
 // If you want to start measuring performance in your app, pass a function
